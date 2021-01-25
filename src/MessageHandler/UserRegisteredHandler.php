1e782c0b36e1bf100cc6705305f6bda4fa691731
vdmkbu <vdmkbu@ya.ru>
2021-01-25 12:15:22 +0500

    add email sending

--- src/MessageHandler/UserRegisteredHandler.php
+++ src/MessageHandler/UserRegisteredHandler.php
@@ -7,17 +7,23 @@ namespace App\MessageHandler;
 use App\Message\UserMessage;
 use App\Repository\UserRepository;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class UserRegisteredHandler implements MessageHandlerInterface
 {
     private LoggerInterface $logger;
     private UserRepository $userRepository;
+    private MailerInterface $mailer;
+    private string $adminEmail;
 
-    public function __construct(LoggerInterface $logger, UserRepository $userRepository)
+    public function __construct(LoggerInterface $logger, UserRepository $userRepository, MailerInterface $mailer, string $adminEmail)
     {
         $this->logger = $logger;
         $this->userRepository = $userRepository;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
     }
 
     public function __invoke(UserMessage $message)
@@ -25,5 +31,13 @@ class UserRegisteredHandler implements MessageHandlerInterface
         $user = $this->userRepository->find($message->getId());
 
         $this->logger->info('User '.$user->getUsername().' registered');
+
+        $this->mailer->send((new NotificationEmail())
+                     ->subject('Welcome to the site')
+                     ->htmlTemplate('emails/welcome.html.twig')
+                     ->from($this->adminEmail)
+                     ->to($user->getEmail())
+                     ->context(['user' => $user])
+        );
     }
 }
